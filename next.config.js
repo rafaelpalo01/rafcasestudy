db52af75777dd5cae859badf68cf46c7fc66604d
Rafael Palo <rafael.palo@accenture.com>
2022-09-13 08:59:57 +0800

    baseurl

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: {
+      BASE_URL: "https://rafcasestudy.vercel.app/",
       MONGO_URI: "mongodb+srv://palo123:peculiar123@cluster0.prpbr97.mongodb.net/?retryWrites=true&w=majority"
   }
 }
