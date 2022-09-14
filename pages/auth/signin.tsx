1c3d9c6b723a7099e59f0a8272257b8a99c64e1b
Rafael Palo <rafael.palo@accenture.com>
2022-09-14 19:50:06 +0800

    asd

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,53 +1,47 @@
-import { NextPage } from 'next';
-import { FormEventHandler, useState } from "react";
+import { NextPage } from "next";
 import { signIn } from "next-auth/react";
-import Link from 'next/link'
+import { FormEventHandler, useState } from "react";
 
 interface Props {}
 
 const SignIn: NextPage = (props): JSX.Element => {
-    const [userInfo, setUserInfo] = useState({email: "", password: ""})
-    const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-        e.preventDefault();
+  const [userInfo, setUserInfo] = useState({ email: "", password: "" });
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    // validate your userinfo
+    e.preventDefault();
 
-        const res = await signIn('credentials', {
-            email: userInfo.email,
-            password: userInfo.password,
-            redirect: false
-        });
+    const res = await signIn("credentials", {
+      email: userInfo.email,
+      password: userInfo.password,
+      redirect: false,
+    });
 
-        console.log(res);
-    };
-    return (
-    <div className='pb-20 bg-slate-200'>
-        <form onSubmit={handleSubmit}>
-            <h1 className='pt-10 pb-6 text-center font-bold text-3xl'>Log In</h1>
-            <div className='text-center'>
-                <div className='text-lg text-center'>Email:</div>
-                <input className='text-lg text-center'
-                value={userInfo.email}
-                onChange={({ target }) =>
-                    setUserInfo({ ...userInfo, email: target.value})
-                } 
-                type='email' 
-                placeholder='raf@gmail.com'/>
-                <div className='text-md text-center'>Password:</div>
-                <input className='text-lg text-center'
-                value={userInfo.password}
-                onChange={({ target }) =>
-                    setUserInfo({ ...userInfo, password: target.value})
-                } 
-                
-                type='password' 
-                placeholder='********'/>
-                <div className='text-md text-center mt-6'></div>
-                <input className='text-lg rounded bg-black text-white cursor-pointer p-1'
-                type='submit' 
-                placeholder='Login'/>
-            </div>
-        </form>
+    console.log(res);
+  };
+  return (
+    <div className="sign-in-form">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <input
+          value={userInfo.email}
+          onChange={({ target }) =>
+            setUserInfo({ ...userInfo, email: target.value })
+          }
+          type="email"
+          placeholder="john@email.com"
+        />
+        <input
+          value={userInfo.password}
+          onChange={({ target }) =>
+            setUserInfo({ ...userInfo, password: target.value })
+          }
+          type="password"
+          placeholder="********"
+        />
+        <input type="submit" value="Login" />
+      </form>
     </div>
-    );
-    
+  );
 };
+
 export default SignIn;
