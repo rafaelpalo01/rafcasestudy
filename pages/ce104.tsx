b551a1a2a56ff70ea45500ff6adc8975254a710f
Rafael Palo <rafael.palo@accenture.com>
2022-09-15 15:21:21 +0800

    asd

--- pages/ce104.tsx
+++ pages/ce104.tsx
@@ -6,12 +6,7 @@ import { useSession } from 'next-auth/react';
 import { useEffect } from 'react';
 import Router from "next/router";
 
-const electricalSupervise: NextPage = (): JSX.Element => {
-  const { status, data } = useSession();
-    useEffect(() => {
-        if(status === "unauthenticated") Router.replace("/auth/signin");
-    }, [status]);
-     if(status === "authenticated")
+const electricalSupervise: NextPage = () => {
   return (
     <div className='pb-20'>
       <div className="md:grid md:grid-cols-2 border-b-2 border-gray-300 pb-6 md:pt-36 pt-10">
